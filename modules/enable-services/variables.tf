cd4cdc128447932dfc6d3952a0bca57d3fa617c1
Fernando Souza <fernhtls@gmail.com>
2026-04-13 10:45:12 +0200

    fix on var validation

--- modules/enable-services/variables.tf
+++ modules/enable-services/variables.tf
@@ -2,7 +2,7 @@ variable "gcp_services" {
   type        = list(string)
   description = "List of services to be enabled"
   validation {
-    condition     = can(length(var.gcp_services) > 0)
+    condition     = length(var.gcp_services) > 0
     error_message = "no services to enable"
   }
 }
